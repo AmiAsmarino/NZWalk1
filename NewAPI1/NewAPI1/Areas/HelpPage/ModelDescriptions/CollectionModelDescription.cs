e52fdeaaeb9e47221423b2714b0b60748b440368
AmiAzmarino <amelonbe9@gmail.com>
2022-12-20 14:28:47 +0100

    Initial files

---
+++ NewAPI1/NewAPI1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace NewAPI1.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
